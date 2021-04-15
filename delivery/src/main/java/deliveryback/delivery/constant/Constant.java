55266838487fede433626f28c222c9adc3f642d9
HUMBERTO GONZALEZ ALFARO <humberto.gonzalez.a@mail.pucv.cl>
2021-04-15 13:33:30 -0400

    add models

---
+++ delivery/src/main/java/deliveryback/delivery/constant/Constant.java
@@ -0,0 +1,10 @@
+package deliveryback.delivery.constant;
+
+public class Constant {
+
+	public static final float TAXES_FOOD = (float) 0.13;
+	
+	public static final float TAXES_BEVERAGE = (float) 0.27;
+	
+	public static final float TAXES_ELECTRONICS = (float) 0.2;
+}
---
+++ delivery/src/main/java/deliveryback/delivery/models/Product.java
@@ -0,0 +1,6 @@
+package deliveryback.delivery.models;
+
+public interface Product {
+
+	public boolean ValidateCreate(int purcheValue, int salesValue);
+}
